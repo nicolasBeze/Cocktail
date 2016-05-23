fb4ea1a263ff9a8d14e3424a57df077157c09ca1
nicolasBeze <nbeze@oxygem.tv>
2016-05-23 15:09:44 +0200

    js

--- src/AppBundle/Resources/public/js/cocktail.js
+++ src/AppBundle/Resources/public/js/cocktail.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
 
+    var dcpt;
+    var timer;
+
     $( "#buttonFormAdd" ).click(function() {
 
         if($('#formAdd').css('display') == 'none'){
@@ -10,6 +13,9 @@ $(document).ready(function() {
     });
 
     $( "#makeCocktail" ).click(function() {
+        dcpt = 22;
+        $('#compte_a_rebours').css("display", "block");
+        timer = setInterval(function(){ decompte() }, 1000);
         id = $('#idCocktail').val();
         $.ajax({
             url: Routing.generate('makeCocktail', {cocktail: id}),
@@ -17,6 +23,27 @@ $(document).ready(function() {
             success: function () {
                 alert('cocktail terminée');
             }
-        })
+        });
+        $('#compte_a_rebours').css("display", "none");
     });
+    
+
+    function decompte()
+    {
+        var affiche = '';
+
+        if (dcpt > 1) {
+            affiche = 'Cocktail prêt dans ' + dcpt + ' secondes';
+        }
+        else {
+            affiche = 'Cocktail prêt dans ' + dcpt + ' seconde';
+        }
+        $("#compte_a_rebours").html(affiche);
+        dcpt -= 1;
+        if (dcpt < 0) {
+            clearInterval(timer);
+        }
+
+    }
+
 });
